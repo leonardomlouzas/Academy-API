94b211ece301b96941d2ce0f43c8dd87721ac6d7
LuizPombo <luizhfpr@gmail.com>
2022-05-02 14:28:47 -0300

    feat: exercise routes integrated

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,10 +3,12 @@ from flask import Blueprint, Flask
 from .home_route import bp as bp_home
 from .personal_route import bp as bp_personal
 from .equipment_route import bp as bp_equipment
+from .exercise_route import bp as bp_exercise
 
 
 def init_app(app: Flask):
     app.register_blueprint(bp_home)
     app.register_blueprint(bp_personal)
     app.register_blueprint(bp_equipment)
+    app.register_blueprint(bp_exercise)
 
