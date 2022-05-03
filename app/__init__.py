2cf9bea2e5158aaf502eb3524666ea881a283f85
Thiago <thiago.dias.148116@gmail.com>
2022-05-03 19:10:16 -0300

    Docs: install CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
+from flask_cors import CORS
 from app import routes
 from app.configs import database, jwt, migration
 
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
+    
     database.init_app(app)
     migration.init_app(app)
     jwt.init_app(app)
