07f806fe08fbe97ed79a4550988fc48dbecefb1a
Guilherme Lima Desoler <guilherme.desoler@gmail.com>
2022-05-03 11:11:55 -0300

    fix: training routes path.

--- app/routes/treino_route.py
+++ app/routes/treino_route.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 
 from app.controllers import treino_controller
 
-bp = Blueprint("training", __name__, url_prefix="/training")
+bp = Blueprint("training", __name__, url_prefix="/treino")
 
 bp.post("")(treino_controller.create)
-bp.patch("/<training_id>")(treino_controller.update)
-bp.delete("/<training_id>")(treino_controller.delete)
-bp.get("/<training_id>")(treino_controller.access_by_id)
+bp.patch("/<treino_id>")(treino_controller.update)
+bp.delete("/<treino_id>")(treino_controller.delete)
+bp.get("/<treino_id>")(treino_controller.access_by_id)
 bp.get("")(treino_controller.access)
